c4f490fc1779c41538760f04eb7bdf252b125bf3
Alejandro <alejandro1000003@gmail.com>
2025-01-06 18:04:56 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // Configura el nombre de la carpeta de salida (opcional, por defecto es "dist")
+  },
   plugins: [react()],
     server: {
     // No especificar un puerto fijo
